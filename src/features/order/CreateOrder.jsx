f5a72135c3a1846b62739adbd9f6f3d64f21b790
ahmedasad236 <ahmedasad236@gmail.com>
2023-07-06 01:03:10 +0300

    Handle phone error using hook useActionData

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Form, redirect, useNavigation } from 'react-router-dom';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
@@ -35,6 +35,7 @@ const fakeCart = [
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
+  const formErrors = useActionData();
   const isSubmitting = navigation.state === 'submitting';
   const cart = fakeCart;
 
@@ -60,6 +61,7 @@ function CreateOrder() {
               name="phone"
               required
             />
+            {formErrors?.phone && <p>{formErrors.phone}</p>}
           </div>
         </div>
 
@@ -109,6 +111,13 @@ export async function action({ request }) {
     priority: data.priority === 'on'
   };
 
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      'Please Give us a correct phone number, we might need it to contact you';
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is okay, create new order and redirect
   const newOrder = await createOrder(order);
   return redirect(`/order/${newOrder.id}`);
 }
