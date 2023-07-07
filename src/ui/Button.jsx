f0430ca911f0baab9c7738019d3eb431b263f261
ahmedasad236 <ahmedasad236@gmail.com>
2023-07-07 11:32:18 +0300

    Finish the style of the Cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,7 +7,9 @@ function Button({ children, disabled, to, type, clickHandler }) {
 
   const styles = {
     primary: base + ' px-4 py-3 md:px-6 md:py-4',
-    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-sm'
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-sm',
+    secondary:
+      'inline-block rounded-full border-2 border-stone-400 font-semibold uppercase tracking-wide text-stone-500 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:text-stone-800 focus-ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5'
   };
   if (to)
     return (
