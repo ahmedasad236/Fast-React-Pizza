19ab4c875f6cd6341a18a078bce0e316d6bb595f
ahmedasad236 <ahmedasad236@gmail.com>
2023-07-08 16:17:55 +0300

    Create the userSlice to store the username

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Username() {
+  const username = useSelector((store) => store.user.username);
+  if (!username) return null;
   return (
-    <div className='hidden text-sm font-semibold md:block'>Ahmed Asaad</div>
-  )
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
-export default Username
+export default Username;
