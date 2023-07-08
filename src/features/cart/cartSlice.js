827650f6b662f5212bd84e588ae0a2eb4fbc73ec
ahmedasad236 <ahmedasad236@gmail.com>
2023-07-08 16:47:19 +0300

    Create the cart slice and append it to the store

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,57 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  cartItems: [
+    {
+      pizzaId: 12,
+      name: 'Mediterranean',
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32
+    }
+  ]
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cartItems.push(action.payload);
+    },
+
+    deleteItem(state, action) {
+      state.cartItems = state.cartItems.filter(
+        (item) => item.pizzaId !== action.payload
+      );
+    },
+
+    increaseItemQuantity(state, action) {
+      const selectedItem = state.cartItems.find(
+        (item) => item.pizzaId === action.payload
+      );
+      selectedItem.quantity++;
+      selectedItem.totalPrice = selectedItem.quantity * selectedItem.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      const selectedItem = state.cartItems.find(
+        (item) => item.pizzaId === action.payload
+      );
+      selectedItem.quantity--;
+      selectedItem.totalPrice = selectedItem.quantity * selectedItem.unitPrice;
+    },
+    clearCart(state) {
+      state.cartItems = [];
+    }
+  }
+});
+
+const {
+  addItem,
+  deleteItem,
+  decreaseItemQuantity,
+  increaseItemQuantity,
+  clearCart
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
