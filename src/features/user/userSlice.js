f637a5936c09e6255d92834c70ca12d0b59a305a
ahmedasad236 <ahmedasad236@gmail.com>
2023-07-09 18:07:15 +0300

    Create a thunk, to fetch user address and position

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,15 +1,18 @@
-/*function getPosition() {
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getAddress } from '../../services/apiGeocoding';
+
+function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 }
 
-async function fetchAddress() {
+const fetchAddress = createAsyncThunk('user/fetchAddress', async function () {
   // 1) We get the user's geolocation position
   const positionObj = await getPosition();
   const position = {
     latitude: positionObj.coords.latitude,
-    longitude: positionObj.coords.longitude,
+    longitude: positionObj.coords.longitude
   };
 
   // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
@@ -18,13 +21,14 @@ async function fetchAddress() {
 
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
-}
-*/
-
-import { createSlice } from '@reduxjs/toolkit';
+});
 
 const initialState = {
-  username: ''
+  username: '',
+  status: 'idle',
+  position: {},
+  address: '',
+  error: ''
 };
 
 const userSlice = createSlice({
@@ -34,7 +38,21 @@ const userSlice = createSlice({
     updateName(state, action) {
       state.username = action.payload;
     }
-  }
+  },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.status = 'idel';
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = 'error';
+        state.error = action.payload.message;
+      })
 });
 
 export const { updateName } = userSlice.actions;
